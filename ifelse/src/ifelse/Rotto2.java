fa39a849ad2db96f683867cd53e163c6a8083e0c
user <user@DESKTOP-2PGRMF4>
2023-03-31 08:24:03 +0900

    ifelse

---
+++ ifelse/src/ifelse/Rotto2.java
@@ -0,0 +1,19 @@
+package ifelse;
+
+public class Rotto2 {
+
+	public static void main(String[] args) {
+
+		int r = 0;
+		while(true) {
+		int rotto = (int)(Math.random()*45+1);
+		System.out.print(rotto+" ");
+		
+		r++;
+		if(r==5) {
+			break;
+		}
+		}
+	}
+
+}
